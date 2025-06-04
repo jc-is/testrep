9d694e388563b75b5c9d35870fc3c090118a477f
jc-is <jc.integrasolucoes@gmail.com>
2025-06-04 13:11:45 -0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,23 @@
-// Carrega os parciais de layout e sections
 async function loadLayoutAndSections() {
-  const layoutRes = await fetch('/partials/layout.html');
-  const layoutHTML = await layoutRes.text();
-  document.getElementById('layout').innerHTML = layoutHTML;
+  // Header
+  const headerRes = await fetch('/partials/header.html');
+  const headerHTML = await headerRes.text();
+  document.getElementById('header').innerHTML = headerHTML;
 
+  // Footer
+  const footerRes = await fetch('/partials/footer.html');
+  const footerHTML = await footerRes.text();
+  document.getElementById('footer').innerHTML = footerHTML;
+
+  // Seções
   const sectionsRes = await fetch('/partials/sections.html');
   const sectionsHTML = await sectionsRes.text();
   document.getElementById('sections').innerHTML = sectionsHTML;
 }
 
-// Aguarda o carregamento completo antes de renderizar os cards
 async function init() {
   await loadLayoutAndSections();
-  await import('./renderCards.js'); // Chama o render após DOM estar pronto
+  await import('./renderCards.js');
 }
 
 init();
